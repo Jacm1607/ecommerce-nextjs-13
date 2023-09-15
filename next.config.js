74357ff356acd790cf2485c1cbfe49a1aa218893
Jose <chavarriaj84@gmail.com>
2023-09-15 00:04:17 -0400

    v1.4

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
     images: {
         domains: ['via.placeholder.com', 'placehold.co', 'www.dashboard.hauscenter.com.bo']
     },
+    assetPrefix: 'https://www.stage.hauscenter.com.bo',
 }
 
 module.exports = nextConfig
